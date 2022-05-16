a31bf77a03e6e713d4fe4802757373c622ae4ab2
Janek9709 <jan.biniek97@gmail.com>
2022-05-16 22:36:17 +0200

    fix: onclick working every click

--- src/App.js
+++ src/App.js
@@ -4,18 +4,11 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
-const [message, setMessage] = useState(null);
-
-useEffect(() => {
-  if(message !== null)
-    alert(message);
-}, [message])
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={() => setMessage("hello react")}>ClickMe!</button>
+        <button onClick={() => alert("hello react")}>ClickMe!</button>
         <a href='https://github.com/Janek9709/React-kurs'>LinkDoRepo</a>
       </header>
     </div>
