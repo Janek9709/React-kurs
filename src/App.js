4115875a9e792d982b30a15421b5c868cfc2d8bd
Janek9709 <jan.biniek97@gmail.com>
2022-05-16 22:12:41 +0200

    feat: added link to repo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <button onClick={() => alert("hello react")}>ClickMe!</button>
+        <a href='https://github.com/Janek9709/React-kurs'>LinkDoRepo</a>
       </header>
     </div>
   );
